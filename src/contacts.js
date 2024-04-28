23e8a75b37eb084f4f7c72f9001946a186e75fad
Saref111 <saref012@gmail.com>
2024-04-28 09:24:56 +0300

    Refactor contacts.js and index.js to improve code readability and maintainability

--- src/contacts.js
+++ src/contacts.js
@@ -1,22 +1,32 @@
 import { promises as fs } from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const contactsPath = path.join(__dirname, "db/contacts.json");
 
 export async function listContacts() {
   const list = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(list);
 }
 
 export async function getContactById(contactId) {
   const contacts = await listContacts();
-  const contact = [contacts].find(({ id }) => id === contactId);
+  const contact = contacts.find(({ id }) => id === contactId);
   return contact || null;
 }
 
 export async function removeContact(contactId) {
   const contacts = await listContacts();
+  const removedContact = contacts.find(
+    ({ id }) => id.toString() === contactId.toString()
+  );
   const newContacts = contacts.filter(({ id }) => id !== contactId);
   await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+  return removedContact || null;
 }
 
 export async function addContact(name, email, phone) {
@@ -24,4 +34,5 @@ export async function addContact(name, email, phone) {
   const newContact = { id: contacts.length + 1, name, email, phone };
   const newContacts = [...contacts, newContact];
   await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+  return newContact;
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import { program } from "commander";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "./contacts.js";
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -10,23 +17,28 @@ program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      const contacts = await listContacts();
+      console.table(contacts);
       break;
 
     case "get":
-      // ... id
+      const contact = await getContactById(id);
+      console.log(contact);
       break;
 
     case "add":
-      // ... name email phone
+      const newContact = await addContact(name, email, phone);
+      console.log("Contact has been added:");
+      console.log(newContact);
       break;
 
     case "remove":
-      // ... id
+      const removedContact = await removeContact(id);
+      console.log("Contact has been removed");
+      console.log(removedContact);
       break;
 
     default:
